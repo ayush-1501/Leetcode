47159a66586f8b9a880cecd8200d503de076dbc5
ayush-1501 <74817479+ayush-1501@users.noreply.github.com>
2022-07-06 01:12:13 +0530

    Time: 284 ms (50.28%), Space: 50.6 MB (30.28%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,24 +1,71 @@
 class Solution {
 public:
-    int dfs(unordered_map<int,int>& mp, int i){
-        int res = 0;
-        if(mp.find(i+1) != mp.end() && mp[i+1] == 0){
-            mp[i+1] = 1;
-            res += 1 + dfs(mp, i+1);
-        }
-        if(mp.find(i-1) != mp.end() && mp[i-1] == 0){
-            mp[i-1] = 1;
-            res += 1 + dfs(mp, i-1);
-        }
-        return res;
+    int parent[100001], Rank[100001]; // make parent and rank array of max size
+    
+    int find(int a) // find function used to tell us the parent of the value 'a'
+    {
+        if(parent[a] == a)
+            return a;
+        
+        return parent[a] = find(parent[a]);
     }
-    int longestConsecutive(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        int ans = 1;
-        for(int i : nums) mp[i] = 0;
-        for(int i: nums){
-            if(mp[i] == 0) ans = max(ans, dfs(mp, i));
+    
+    void Union(int a, int b) // By union we are making parent 
+    {
+        a = find(a); // find parent of a
+        b = find(b); // find parent of b
+        
+        if(a == b) // if both parents are equal, simply return
+            return;
+        
+        if(Rank[a] > Rank[b]) // rank of a is larger, so it will become parent
+        {
+            // a would be parent
+            parent[b] = a; 
+            Rank[a] += Rank[b]; // a become parent, so increase it's rank
         }
-        return nums.size()?ans:0;
+        else
+        {
+            // b would be parent
+            parent[a] = b;
+            Rank[b] += Rank[a]; // b become parent, so increase it's rank
+        } 
     }
-};
+    int longestConsecutive(vector<int>& arr) {
+        int n = arr.size(); // extract the size of array
+        if(n == 0) return 0; // if length is zero simply return zero
+        
+        // since our parent and rank are global array so everytime reintiliaze them
+        for(int i = 0; i < n; i++) parent[i] = i, Rank[i] = 1;
+        
+        unordered_map<int, int> mp; // making map
+        
+        for(int i = 0; i < n; i++) // traverse from the arrat
+        {
+            // if element is already presnt into the map, just continue
+            if(mp.find(arr[i]) != mp.end()) continue;
+            
+            // if (arr[i] - 1) present in the map, we will perform union
+            if(mp.find(arr[i] - 1) != mp.end())
+            {
+                Union(mp[arr[i] - 1], i);
+            }
+            
+             // also, if (arr[i] - 1) present in the map, we will perform union
+            if(mp.find(arr[i] + 1) != mp.end())
+            {
+                Union(mp[arr[i] + 1], i);
+            }
+            
+             mp[arr[i]] = i;
+        }
+        
+        int ans = 0; // intiliaze ans as zero
+        for(int i = 0; i < n; i++)
+        {
+            ans = max(ans, Rank[i]); // take the largest component
+        }
+        
+        return ans; // finally return ans
+    }
+}; 
