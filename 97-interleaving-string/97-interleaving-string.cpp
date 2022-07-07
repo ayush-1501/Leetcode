05f4d851f89e50686a0eff57904e5e6166b05968
ayush-1501 <74817479+ayush-1501@users.noreply.github.com>
2022-07-08 00:40:33 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (50.22%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     
-bool helper(string &s1, string &s2, string &s3, int i, int j, int k, vector<vector<int>> &dp)
-{
-    if(i == s1.size() && j == s2.size())
-        return k == s3.size();
-    
-    if(dp[i][j] != -1)
-        return dp[i][j];
-    
-    bool flag = false;
-    if(i < s1.size() && k < s3.size() && s1[i] == s3[k])
-        flag = helper(s1, s2, s3, i + 1, j, k + 1, dp);
-    
-    if(!flag && j < s2.size() && k < s3.size() && s2[j] == s3[k])
-        flag = helper(s1, s2, s3, i, j + 1, k + 1, dp);
-    
-    return dp[i][j] = flag;
-}    
-   bool isInterleave(string s1, string s2, string s3) {
-
-    vector<vector<int>> dp(s1.size() + 1, vector<int> (s2.size() + 1, -1));
-    return helper(s1, s2, s3, 0, 0, 0, dp);
-  }
+bool f(int i, int j, int k, string& s1, string& s2, string& s3, vector<vector<int>>& dp) {
+        if (i == s1.length() && j == s2.length() && k == s3.length()) {
+            return dp[i][j] = true;
+        }
+        if (dp[i][j] != -1) return dp[i][j];
+        if (s3[k] == s1[i] && s3[k] != s2[j]) {
+            return dp[i][j] = f(i + 1, j, k + 1, s1, s2, s3, dp);
+        } else if (s3[k] == s2[j] && s3[k] != s1[i]) {
+            return dp[i][j] = f(i, j+1, k+1, s1, s2, s3, dp);
+        } else if (s3[k] == s1[i] && s3[k] == s2[j]) {
+            return dp[i][j] = f(i, j+1, k+1, s1, s2, s3, dp) || f(i + 1, j, k + 1, s1, s2, s3, dp);
+        }
+        return dp[i][j] = false;
+    }
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        if (s1.size() + s2.size() != s3.size()) return false;
+        vector<vector<int>> dp(s1.length() + 1, vector<int>(s2.length() + 1, -1));
+        return f(0, 0, 0, s1, s2, s3, dp);
+    }
 };
