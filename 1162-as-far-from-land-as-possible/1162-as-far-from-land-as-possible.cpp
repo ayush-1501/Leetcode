862b1c32a726b5ef1447715cd20e94e695ae356f
ayush-1501 <74817479+ayush-1501@users.noreply.github.com>
2022-07-10 14:58:49 +0530

    Time: 90 ms (67.92%), Space: 21.4 MB (41.73%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -7,42 +7,43 @@ class Solution
   public:
     int maxDistance(vector<vector<int>>& grid) 
     {
-        queue<std::pair<int, int>>q;
+        queue<pair<int, int>>q;
+        //creating a 2D dp of grid.size()xgrid.size()
         vector<std::vector<int>> dp(grid.size(), std::vector<int>(grid.size(), INT_MAX));
-        for (int i = 0; i < grid.size(); i++)
-        {
-            for (int j = 0; j < grid.size(); j++)
-            {
-                if (grid[i][j] == 1)
-                {
+       
+        //storing all the points [i][j] from where bfs would be done
+        for (int i = 0; i < grid.size(); i++){
+            for (int j = 0; j < grid.size(); j++){
+                if (grid[i][j] == 1){
                     dp[i][j] = 0;
                     q.push({i, j});
                 }
             }
         }
+        
+        //for moving either to adjacent left ,right,up or down
         vector<pair<int,int>>dir={{0,-1},{-1,0},{0,1},{1,0}};
-        while (!q.empty())
-        {
+        while (!q.empty()) {
             pair<int, int>p= q.front();
             q.pop();
             
-            for (auto k:dir)
-            {
-                int next_r = p.first + k.first;
-                int next_c = p.second + k.second;
+            for (auto k:dir){
+                int x = p.first + k.first;
+                int y = p.second + k.second;
                 
-                if (isValid(grid.size(), next_r, next_c) && grid[next_r][next_c] == 0)
-                {
-                    if (1 + dp[p.first][p.second] < dp[next_r][next_c])
-                    {
-                        dp[next_r][next_c] = 1 + dp[p.first][p.second];
-                        q.push({next_r, next_c});
+                //checking if the nect co-ordinate [x][y] is valid or not
+                if (isValid(grid.size(),x,y) && grid[x][y] == 0){
+                    //minimizing the distance from 1 to all the reachable 0's
+                    if (1 + dp[p.first][p.second] < dp[x][y]){
+                        dp[x][y] = 1 + dp[p.first][p.second];
+                        q.push({x,y});
                     }
                 }
             }
         }
         
         int res = -1;
+        //finding maximum of all the distances
         for (int i = 0; i < grid.size(); i++){
             for (int j = 0; j < grid.size(); j++){
                 if (dp[i][j] != 0 && dp[i][j] != INT_MAX){
