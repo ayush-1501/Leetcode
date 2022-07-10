7fef8c1f10b5d4f008fb571dec0b5916ffff7032
ayush-1501 <74817479+ayush-1501@users.noreply.github.com>
2022-07-10 13:32:39 +0530

    Time: 37 ms (8.44%), Space: 13.9 MB (8.48%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,33 +1,62 @@
 class Solution {
 public:
-    void dfs(int i,int j,vector<vector<int>>& grid,int N,int M){
-        if(i<0 or j<0 or i>=N or j>=M or grid[i][j]==1)return;
-        
-        grid[i][j]=1;
-        dfs(i+1,j,grid,N,M);
-        dfs(i,j+1,grid,N,M);
-        dfs(i-1,j,grid,N,M);
-        dfs(i,j-1,grid,N,M);     
-    }
+    vector<pair<int,int>>dir{{0,1},{1,0},{0,-1},{-1,0}};
     int closedIsland(vector<vector<int>>& grid) {
         int N=grid.size();
         int M=grid[0].size();
-        
-       for(int i = 0;i<N;i++){
-            for(int j = 0;j<M;j++){
-                if(grid[i][j] == 0 and (i == 0 or j==0 or i==N-1 or j==M-1))
-                    dfs(i,j,grid,N,M);
-            }
-        }
-        int count=0;
+        int c=0;
         for(int i=0;i<N;i++){
             for(int j=0;j<M;j++){
-                if(grid[i][j]==0){
-                    dfs(i,j,grid,N,M);
-                    count++;
+              if((i==0 or j==0 or i==N-1 or j==M-1) and grid[i][j]==0){
+                queue<pair<int,int>>q;
+                q.push({i,j});
+                while(!q.empty()){
+                    int n=q.size();
+                    while(n--){
+                        pair<int,int>p=q.front();
+                        q.pop();
+                        grid[p.first][p.second]=1;
+                        for(auto k:dir){
+                            int x=p.first+k.first;
+                            int y=p.second+k.second;
+                            
+                            if(x>=0 and x<N and y>=0 and y<M and grid[x][y]==0){
+                                grid[x][y]=1;
+                                q.push({x,y});
+                            }
+                        }
+                    }
                 }
-            }
+              }   
+            }    
         }
-        return count;
+        
+
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+              if(grid[i][j]==1){continue;}
+                queue<pair<int,int>>q;
+                q.push({i,j});
+                c++;
+                while(!q.empty()){
+                    int n=q.size();
+                    while(n--){
+                        pair<int,int>p=q.front();
+                        q.pop();
+                        grid[p.first][p.second]=1;
+                        for(auto k:dir){
+                            int x=p.first+k.first;
+                            int y=p.second+k.second;
+                            
+                            if(x>=0 and x<N and y>=0 and y<M and grid[x][y]==0){
+                                grid[x][y]=1;
+                                q.push({x,y});
+                            }
+                        }
+                    }
+                }
+            }    
+        }  
+        return c;
     }
 };
