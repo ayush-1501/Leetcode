7a49b1c1d01b8fe340c1b0d8d45127dd90cfd8db
ayush-1501 <74817479+ayush-1501@users.noreply.github.com>
2022-07-14 13:16:57 +0530

    Time: 25 ms (5.45%), Space: 18.7 MB (5.18%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    void f(int i,vector<int>&nums,vector<int>&curr){
-        if(i==nums.size()){
-            ans.push_back(curr);
-            return;
-        }
-       curr.push_back(nums[i]);
-        f(i+1,nums,curr);
-        if(!curr.empty())curr.pop_back();
-        if(i+1<=nums.size()){i++;}
-        while(i<nums.size() and i>0 and nums[i]==nums[i-1]){i++;}
-         f(i,nums,curr);   
+   set<vector<int>> s;
+
+void solve(vector<int> nums, int i, vector<int> path){
+    
+    if(i == nums.size()){
+        s.insert(path);
+        return ;
     }
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<int>curr;
-        f(0,nums,curr);
-        return ans;
+    
+    
+    path.push_back(nums[i]);
+    solve(nums, i+1, path);
+    path.pop_back();
+    solve(nums, i+1, path);
+    
+}
+
+vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+    
+    vector<vector<int>> res;
+    sort(nums.begin(), nums.end());
+    
+    solve(nums, 0, {});
+    
+    
+    for(auto i : s){
+        res.push_back(i);
     }
+    
+    return res;
+ }
 };
