43ebaac99d25dccd96d5015f4e9864ca0d5e5673
ayush-1501 <74817479+ayush-1501@users.noreply.github.com>
2022-07-21 20:52:19 +0530

    Time: 4 ms (98.15%), Space: 16.1 MB (18.46%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.cpp
+++ 526-beautiful-arrangement/526-beautiful-arrangement.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
-    typedef long long ll;
-    ll res = 0;
-    void func(int pos,int used, int& n) {
-        if (pos==n+1) {
-            ++res;
-            return;
-        }
-        for (int i=1;i<=n;++i) {
-            if (!((pos%i && i%pos)||(used&(1<<i)))) {
-                func(pos+1,used^(1<<i), n);
+     bool checkValid( int i , int idx , int arragement){
+        if( ( i % idx == 0 or idx % i == 0 ) and ((1 << idx) & arragement) == 0 ) return true;
+        return false;
+    }
+    
+    
+    int helper( int i , int n , int arragement, vector<vector<int>>& dp ){
+        // base case
+        if( i == 1 ) return 1;
+        
+        if( dp[i][arragement] != -1 ) return dp[i][arragement];
+        
+        // recursive case
+        int sum = 0;
+        for( int idx = 1 ; idx <= n ; idx++ ){
+            if( checkValid( i , idx , arragement) ){
+                arragement = arragement | (1 << idx);
+                sum += helper( i-1 , n , arragement , dp);
+                arragement = arragement ^ (1 << idx);
             }
         }
+        
+        return dp[i][arragement] = sum;
     }
+    
+    
     int countArrangement(int n) {
-        func(1, 0, n);
-        return res;
+        if(n<4)return n;
+        vector<vector<int>>dp(n+1,vector<int>(pow(2,n+1),-1));
+        int arragement = 0;
+        return helper( n , n , arragement, dp);
     }
 };
