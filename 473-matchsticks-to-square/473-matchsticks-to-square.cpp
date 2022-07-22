5fa74d99e0d25dd194ab3faf9462a32c00909436
ayush-1501 <74817479+ayush-1501@users.noreply.github.com>
2022-07-23 00:22:59 +0530

    Time: 792 ms (24.93%), Space: 10.2 MB (48.02%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-   bool dfs(int index, vector<int>& sideLength, int target, vector<int>&sti){
+   bool dfs(int index,vector<int>& sidel,int target,vector<int>&sti){
         if(index == sti.size()){
-            return sideLength[0] == sideLength[1] && sideLength[1] == sideLength[2] && sideLength[2] == sideLength[3];
+          return sidel[0]==sidel[1]&&sidel[1]==sidel[2]&&sidel[2]==sidel[3];
         }
         
         for(int i = 0; i < 4; i++){
-            if(sideLength[i] + sti[index] > target)continue;
+            if(sidel[i]+sti[index]>target)continue;
+            //int j = i;
+            // while(--j >= 0){
+            //     if(sidel[i] == sidel[j])break;
+            // }
+            // if(j != -1)continue;
             
-			
-            int j = i;//Second Optimisation if the same length is present don't need to check again
-            while(--j >= 0){
-                if(sideLength[i] == sideLength[j])break;
-            }
-            if(j != -1)continue;
-            
-            sideLength[i] += sti[index];
-            if(dfs(index+1, sideLength, target, sti)){
+            sidel[i]+=sti[index];
+            if(dfs(index+1,sidel,target,sti)){
                 return true;
             }
-            sideLength[i] -= sti[index];
+            sidel[i]-=sti[index];
         }
         
         return false;
@@ -30,13 +28,12 @@ public:
         for(auto it : sti)sum+=it;
         
         if(sum%4 != 0)return false;
-        //First Optimisation
         sort(sti.begin(), sti.end(), greater<int>());
         if(sti[0] > sum/4)return false;
         
         int target = sum/4;
-        vector<int>sideLength(4, 0);
-        return dfs(0, sideLength, target, sti);
+        vector<int>sidel(4, 0);
+        return dfs(0, sidel, target, sti);
             
     }
 };
