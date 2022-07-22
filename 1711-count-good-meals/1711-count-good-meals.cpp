05e2448de68bc4449c2092069e19c60d181064a3
ayush-1501 <74817479+ayush-1501@users.noreply.github.com>
2022-07-23 00:49:30 +0530

    Time: 504 ms (69.44%), Space: 58 MB (71.90%) - LeetHub

---
+++ 1711-count-good-meals/1711-count-good-meals.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countPairs(vector<int>&nums) {
+         int n=nums.size();
+         long long ans=0;
+         unordered_map<int,int> m;
+          for(auto &i:nums){
+           for(int j=0;j<=21;++j){
+               int cal=1<<j;
+            if(m.find(cal-i)!=m.end()){
+                ans=ans +m[cal-i];
+             }
+           } 
+          m[i]++;
+        }
+     return (int)(ans%1000000007);
+    }
+};
