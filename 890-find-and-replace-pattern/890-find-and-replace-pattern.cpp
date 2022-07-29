f0d4ff71017f2ee4e8b527749e6c8b4cdf374838
ayush-1501 <74817479+ayush-1501@users.noreply.github.com>
2022-07-29 18:18:26 +0530

    Time: 3 ms (90.27%), Space: 8.1 MB (83.72%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -2,27 +2,23 @@ class Solution {
 public:
     vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
         vector<string>ans;
-        for(string s : words){
-            if(s.size()!=pattern.size())continue;
-             vector<char> arr(26,-1);
-             unordered_map<char,int>m;
-             bool flag = true;
-             for(int j = 0;j<s.size();j++){
-                char a=s[j];
-                char b=pattern[j];
-                if((arr[a-'a']==-1 && m.count(b)==0)||arr[a-'a']== b) {
-                    arr[a-'a'] = b;
-                    m[b] = 1;
+        for(string &i:words){
+            if(i.length()!=pattern.length())continue;
+            vector<char>mark(26,-1);
+            bool flag=true;
+            map<char,int>m;
+            for(int j=0;j<i.size();j++){
+                if((mark[i[j]-'a']==-1 and(m.count(pattern[j])==0))or(mark[i[j]-'a']==pattern[j])){
+                    m[pattern[j]]++;mark[i[j]-'a']=pattern[j];
                 }
                 else{
-                   flag = false;
+                    flag=false;
                     break;
-                } 
-             }
-            if(flag){
-                ans.emplace_back(s);
+                }
             }
-            
+            if(flag){
+                ans.emplace_back(i);
+            }
         }
         return ans;
     }
