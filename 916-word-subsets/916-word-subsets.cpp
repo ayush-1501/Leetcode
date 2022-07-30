c1cb8ef8ff9c71fa49034d401593fc94466b3c8a
ayush-1501 <74817479+ayush-1501@users.noreply.github.com>
2022-07-30 23:32:35 +0530

    Time: 227 ms (79.55%), Space: 102.4 MB (40.67%) - LeetHub

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-     unordered_map<char , int> count(string &s){
-        unordered_map<char , int>mm;
-        for(auto i : s)mm[i]++;
-        return mm;
-    }
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
-        unordered_map<char, int> maxFreq;
-        
-        for(auto s : words2) {
-            unordered_map<char, int> f = count(s);
-            for(char c : s)
-                maxFreq[c] = max(maxFreq[c], f[c]);
+          vector<int> count(26), tmp(26);
+        int i;
+        for (string b :words2) {
+            tmp = counter(b);
+            for (i = 0; i < 26; ++i)
+                count[i] = max(count[i], tmp[i]);
         }
-        
-        vector<string> ans;
-        for(auto s : words1) {
-            unordered_map<char, int> temp = maxFreq;
-            for(auto c : s) {
-                if(temp.find(c) == temp.end()) continue;
-                temp[c]--;
-                if(!temp[c]) temp.erase(c);
-            }
-            if(!temp.size()) ans.push_back(s);
+        vector<string> res;
+        for (string a : words1) {
+            tmp = counter(a);
+            for (i = 0; i < 26; ++i)
+                if (tmp[i] < count[i])
+                    break;
+            if (i == 26) res.push_back(a);
         }
-        
-        return ans;
+        return res;
     }
-};  
+
+    vector<int> counter(string& word) {
+        vector<int> count(26);
+        for (char c : word) count[c - 'a']++;
+        return count;
+    }
+};
