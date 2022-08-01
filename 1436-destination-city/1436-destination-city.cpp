54088365283c2bfe551ff882fb2e7587345c89f9
ayush-1501 <74817479+ayush-1501@users.noreply.github.com>
2022-08-01 16:09:34 +0530

    Time: 34 ms (8.83%), Space: 11.7 MB (10.73%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        unordered_map<string,int>mp;
+         for(auto i:paths){
+            mp[i[0]]++;
+        }
+        for(auto i:paths){
+            if(mp[i[1]]==0){
+                return i[1];
+            }
+        }
+        return "-1";
+    }
+};
