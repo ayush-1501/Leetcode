75898867dbc8cac77bb9fb2fa648b9a3b2f0a11f
ayush-1501 <74817479+ayush-1501@users.noreply.github.com>
2022-08-02 21:59:43 +0530

    Time: 69 ms (27.86%), Space: 14.5 MB (24.46%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-       multiset<int>s;
-        for(auto i:matrix){
-            for(int j:i){
-                s.insert(j);
-            }
-        }
-        auto it = s.begin();
-        for (int i=0; i <k-1;i++)
-          it++;
-        return *it;
+        vector<int>store;
+       for (int i = 0; i <matrix.size(); i++){
+           for (int j = 0; j <matrix.size(); j++){
+               store.emplace_back(matrix[i][j]);
+           }
+       }
+       sort(store.begin(),store.end());
+       return store[k-1];
     }
 };
