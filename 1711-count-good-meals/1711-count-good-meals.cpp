910ca3d0c4ff35542d51fe019a362a0f88e2cbe0
Ayush <74817479+ayush-1501@users.noreply.github.com>
2022-09-20 11:46:28 +0530

    Time: 926 ms (35.00%), Space: 58 MB (66.18%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.cpp
+++ 1711-count-good-meals/1711-count-good-meals.cpp
@@ -5,7 +5,7 @@ public:
          long long ans=0;
          unordered_map<int,int> m;
           for(auto &i:nums){
-           for(int j=0;j<=21;++j){
+           for(int j=0;j<31;++j){
                int cal=1<<j;
             if(m.find(cal-i)!=m.end()){
                 ans=ans +m[cal-i];
