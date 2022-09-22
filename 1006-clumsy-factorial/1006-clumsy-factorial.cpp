dbd6ba3cfc4842842ca55d6a848db9b0d6ed16ec
Ayush <74817479+ayush-1501@users.noreply.github.com>
2022-09-22 23:33:26 +0530

    Time: 4 ms (35.14%), Space: 6.9 MB (11.06%) - LeetHub

--- 1006-clumsy-factorial/1006-clumsy-factorial.cpp
+++ 1006-clumsy-factorial/1006-clumsy-factorial.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
     int clumsy(int n) {
+       int ans=0;
+        int add=0;
         if(n<=2){return n;}
         if(n==3){return 6;}
-        if(n==4){return 7;}
-        if(n%4==1){return n+2;}
-        if(n%4==2){return n+2;}
-        if(n%4==3){return n-1;}
-        if(n%4==0){return n+1;}
-        return -1;
+        vector<int>store;
+        while(n>=4){
+            int t=n*(n-1)/(n-2);
+            store.emplace_back(t);
+            add+=(n-3);
+            n-=4;
+        }
+        if(n==3){
+            ans-=n*(n-1)/(n-2);
+        }
+        else if(n==2){
+            ans-=n*(n-1);
+        }else if(n==1){
+            ans-=n;
+        }
+        ans+=store[0];
+        for(int i=1;i<store.size();i++){
+            ans-=store[i];
+        }
+        return ans+add;
     }
 };
