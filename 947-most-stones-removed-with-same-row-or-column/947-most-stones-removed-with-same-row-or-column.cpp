94bf7602d13a8681092acb47ac2d26d51ee45d78
Ayush <74817479+ayush-1501@users.noreply.github.com>
2022-09-27 16:16:33 +0530

    Time: 91 ms (78.04%), Space: 20.9 MB (25.75%) - LeetHub

---
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,44 @@
+const int N=1e4;
+ vector<int>g(N,-1);
+class dsu{
+public:
+   dsu(int n){
+      for(int i=0;i<n;i++)g[i]=i;
+   }
+   int find_par(int i){
+      if(i==g[i])return i;
+      return g[i]=find_par(g[i]);
+   }
+   void uni(int i,int j){
+      int a=find_par(i);
+       int b=find_par(j);
+           g[a]=b;
+           return;
+   }
+};
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        if(stones.size()==1){return 0;}
+        int n=stones.size();
+        dsu stone(n);
+       unordered_map<int,vector<int>> r,c;
+	    for(int i = 0; i < stones.size(); i++) {
+		r[stones[i][0]].push_back(i);
+		c[stones[i][1]].push_back(i);
+	   }
+
+	
+	for(int i=0;i<stones.size();i++) {
+		for(int j:r[stones[i][0]])stone.uni(i,j);
+		for(int j:c[stones[i][1]])stone.uni(i,j);
+	}
+
+        set<int>st;
+        for(int i=0;i<n;i++){
+            st.insert(stone.find_par(i));
+            cout<<g[i]<<" ";
+        }
+        return stones.size()-st.size();
+    }
+};
