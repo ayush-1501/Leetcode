d3f961868193d91c4e19cc494969fa2eed5249e5
Ayush <74817479+ayush-1501@users.noreply.github.com>
2022-09-27 19:12:25 +0530

    Time: 5 ms (23.75%), Space: 7.7 MB (77.77%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int solve(vector<int>&nums,int i,vector<int>&dp){
         if(i>=nums.size()){return 0;}
-        if(i==nums.size()-1){return dp[i]=nums[i];}
         if(dp[i]!=-1){return dp[i];}
         int pick=nums[i]+solve(nums,i+2,dp);
         int notpick=solve(nums,i+1,dp);
@@ -10,6 +9,6 @@ public:
     }
     int rob(vector<int>& nums) {
         vector<int>dp(nums.size()+1,-1);
-        return max(solve(nums,0,dp),solve(nums,1,dp));
+        return solve(nums,0,dp);
     }
 };
