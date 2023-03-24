d41661e73b1dd0ae052f5bd239850a5e20a410a5
Ayush <74817479+ayush-1501@users.noreply.github.com>
2023-03-24 22:06:29 +0530

    Time: 7 ms (68.97%), Space: 10.9 MB (60.07%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+       void findcombo(int ind,int target,vector<int>&arr,vector<vector<int>>&ans,vector<int>&ds){
+        if(ind==arr.size()){
+            if(target==0) 
+                ans.push_back(ds);
+         return;
+        }
+
+    
+        if(arr[ind]<=target){
+            ds.push_back(arr[ind]);
+            findcombo(ind,target-arr[ind],arr,ans,ds); 
+            ds.pop_back();
+        }
+        findcombo(ind+1,target,arr,ans,ds);
+    }
+    
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans; 
+        vector<int>ds;
+        findcombo(0,target,candidates,ans,ds);
+        return ans;
+    }
+};
